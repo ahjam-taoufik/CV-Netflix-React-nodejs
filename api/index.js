8e56a389678db29d22039c4eddd1e59711257bc8
taoufik ahjam <taoufikahjam@gmail.com>
2022-03-31 11:02:21 +0200

    add movies crud

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
 dotenv.config();
 
 mongoose
@@ -21,7 +22,7 @@ mongoose
  
   app.use("/api/auth", authRoute);
   app.use("/api/users", userRoute);
-
+  app.use("/api/movies", movieRoute);
 
 
 app.listen(8800, () => {
