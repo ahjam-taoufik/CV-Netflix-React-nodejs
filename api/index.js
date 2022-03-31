728bd75b80cb3f741178132f80b077326466a983
taoufik ahjam <taoufikahjam@gmail.com>
2022-03-31 11:58:58 +0200

    implement lists of actions

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const dotenv = require('dotenv');
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
+
 dotenv.config();
 
 mongoose
@@ -23,8 +25,9 @@ mongoose
   app.use("/api/auth", authRoute);
   app.use("/api/users", userRoute);
   app.use("/api/movies", movieRoute);
+  app.use("/api/lists", listRoute);
 
 
 app.listen(8800, () => {
-  console.log('server is running');
+  console.log('server is running'); 
 });
